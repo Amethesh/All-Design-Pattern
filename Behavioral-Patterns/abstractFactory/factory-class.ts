774656812c8f7bade3b75337e9cbf8607b8bb177
Amethesh L G P <amethesh.l@ei-india.local>
2024-01-15 15:43:07 +0530

    Implemented abstract factory

---
+++ Behavioral-Patterns/abstractFactory/factory-class.ts
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+interface ILogger {
+    info(message: string): void
+    warn(message: string): void
+    debug(message: string): void
+    error(message: string): void
+}
+
+class ProductionLogger implements ILogger {
+    info(message: string): void {
+        
+    }
+    debug(message: string): void {
+        
+    }
+    warn(message: string): void {
+     console.warn(message)   
+    }
+    
+    error(message: string): void {
+        console.error(message)   
+        
+    }
+}
+
+class DevelopmentLogger implements ILogger {
+    info(message: string): void {
+     console.log(message)
+    }
+    debug(message: string): void {
+     console.debug(message)
+    }
+    warn(message: string): void {
+     console.warn(message)   
+    }
+    
+    error(message: string): void {
+    console.error(message)   
+        
+    }
+}
+
+export class LoggerFactory {
+    public static createLogger():ILogger{
+        if(process.env.PRODUCTION === "production"){
+            return new ProductionLogger();
+        }
+        else {
+            return new DevelopmentLogger();
+        }
+    }
+}
